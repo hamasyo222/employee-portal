9a6fae6d4454cf3c82ec8cd7bca3e54a88399d81
hamasyo222 <hamasyo222@keio.jp>
2025-06-23 23:56:54 +0900

    up

---
+++ src/utils/dummyData.ts
@@ -0,0 +1,9 @@
+import { User } from '../types';
+
+export const dummyUser: User = {
+  id: '1',
+  email: 'test@employee.com',
+  firstName: 'テスト',
+  lastName: 'ユーザー',
+  userType: 'employee',
+}; 
